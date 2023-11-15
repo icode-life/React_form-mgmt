699421b368e7a527680036c7526aa383412ab6bc
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-11-16 00:12:50 +0100

    Form/List mgmt using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+/*this array was used for example at first, before the app was able to manage ne entries from the interface*/
+  const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 1, packed: true },
+];
 
 function App() {
+  /* state lifted up from form component bc it is rendered in a siblig component: packing list */
+  const [items, setItems] = useState([]);
+
+  /*here is the logic that updates the array items. It is here as it touches several sibling components*/
+  /* the form is responsible for updating the array tho, so we nned to give the Form access to this function via the props */
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Form onAddItems={handleAddItems} /> {/**the way the prop i snamed is based on a convention */}
+      <PackingList items={items} />
+      <Stats />
     </div>
   );
 }
 
 export default App;
+
+const Logo = () => {
+  return <h1>🏝️ Far Away 🧳</h1>;
+};
+const Form = ({onAddItems}) => {
+
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = {description, quantity, packed: false, id: Date.now()};
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  return  (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({length: 20}, (_, i) => i+1).map(num => <option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="item to be added..." value={description} onChange={(e) => setDescription(e.target.value)}></input>
+      <button>Add</button>
+    </form>
+  )
+};
+
+const PackingList = ({items}) => {
+  return <div className="list">
+    <ul>
+      {items.map(item => <Item item={item} key={item.id}/>)}
+    </ul>
+  </div>
+};
+
+const Item = ({item}) => {
+  return (
+     <li>
+        <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
+        <button>❌</button>
+    </li>
+    )
+};
+
+const Stats = () => {
+  return <footer className="stats">
+    <em>You have X items on your list, and you already placked X (X%)</em>
+  </footer>
+};
