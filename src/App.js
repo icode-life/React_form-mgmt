2462b8acc8131cac817503060a5da98115fd17da
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-11-16 03:35:01 +0100

    toggle item

--- src/App.js
+++ src/App.js
@@ -18,14 +18,18 @@ function App() {
   };
 
   const handleDeleteItem = (id) => {
-    setItems(items => items.filter(item => item.id !== id))
+    setItems(items => items.filter(item => item.id !== id));
+  };
+
+  const handleToggleItem = (id) =>{
+    setItems(items => items.map(item => item.id === id ? {...item, packed: !item.packed} : item));
   };
 
   return (
     <div className="App">
       <Logo />
       <Form onAddItems={handleAddItems} /> {/**the way the prop i snamed is based on a convention */}
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
       <Stats />
     </div>
   );
@@ -66,17 +70,18 @@ const Form = ({onAddItems}) => {
   )
 };
 
-const PackingList = ({items, onDeleteItem}) => {
+const PackingList = ({items, onDeleteItem, onToggleItem}) => {
   return <div className="list">
     <ul>
-      {items.map(item => <Item item={item} onDeleteItem={onDeleteItem} key={item.id}/>)}
+      {items.map(item => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
     </ul>
   </div>
 };
 
-const Item = ({item, onDeleteItem}) => {
+const Item = ({item, onDeleteItem, onToggleItem}) => {
   return (
      <li>
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
         <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
         <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
